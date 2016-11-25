1c806ec03675652176fe96f378bc55c3e0d7318e
Hasibullah Faroq <Hasib90@gmx.de>
2016-11-25 13:56:51 +0100

    first changes

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -9,4 +9,19 @@ CREATE TABLE Users (
  
 # --- !Downs
  
-DROP TABLE User;
+DROP TABLE Users;
+
+# Menu Schema
+
+# --- !Ups
+
+CREATE TABLE Menu (
+    id serial PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    price int,
+    category varchar(255) NOT NULL
+);
+
+# --- !Downs
+
+DROP TABLE Menu;
