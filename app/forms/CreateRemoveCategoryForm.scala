c073d148970c23a18070973e7eef82af0d5e0bda
BruceWayne-25 <hasibullah.faroq@gmail.com>
2017-04-23 12:07:49 +0200

    Kategorieverwaltung erweitert + forms

---
+++ app/forms/CreateRemoveCategoryForm.scala
@@ -0,0 +1,6 @@
+package forms
+
+/**
+  * Created by Hasib on 21.04.2017.
+  */
+case class CreateRemoveCategoryForm(category: String)
---
+++ app/forms/CreateUpdateCategoryForm.scala
@@ -0,0 +1,6 @@
+package forms
+
+/**
+  * Created by Hasib on 21.04.2017.
+  */
+case class CreateUpdateCategoryForm(newCategory: String, oldCategory: String)
